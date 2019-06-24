ed1df9cdae077029fec3f4540dc057b82c42897a
Daniel Hickmott <daniel.hickmott@gmail.com>
2019-06-24 17:57:43 +1000

    Changes script that renders Scratch blocks to render Scratch 3 blocks

--- js/render-scratch-block.js
+++ js/render-scratch-block.js
@@ -4,11 +4,12 @@ $(document).ready(function() {
 
         $.each(preBlock.children, function(childIndex, codeBlock) {
 
-        // Parse the contennts of the code block with ScratchBlocks library
-        var doc = scratchblocks.parse(codeBlock.innerHTML);
-        doc.render(function (svg) {
-
-            // Create the Div element that the Scratch blocks will go into
+            // Parse the contents of the code block with ScratchBlocks library
+            var doc = scratchblocks.parse(codeBlock.innerHTML);
+            var docView = scratchblocks.newView(doc, { style: 'scratch3' });
+            
+            // Render the Scratch blocks code and replace the code with the SVG
+            var svg = docView.render();
             var answerId = "scratch-" + preIndex + "_" + childIndex;
             var answerDiv = $("<div id=\"" + answerId + "\" class=\"scratch-blocks\"></div>");
             answerDiv[0].appendChild(svg);
@@ -16,8 +17,6 @@ $(document).ready(function() {
             // Add the SVG to the DOM and remove the text Code
             preBlock.after(answerDiv[0]);
             preBlock.remove();
-
-        });
         });
     });
 });
