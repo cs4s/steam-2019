f8a8a3480f79270b34e69e6bdb93c73629de5756
Daniel Hickmott <daniel.hickmott@gmail.com>
2019-07-09 14:59:19 +1000

    Fixes link used in hover over metalanguage js
    Index.html was appended for some reason and no longer worked

--- js/metalanguage-hover.js
+++ js/metalanguage-hover.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 	$.each(metalanguage_terms, function(index, term) {
 
 		var term_id = term.innerText.toLowerCase().replace(/ /g, "-");
-		$(this).prop("href", "../glossary/index.html#" + term_id);
+		$(this).prop("href", "../glossary#" + term_id);
 
 		term.id = "metalanguage-" + term_id 
 		term_ids.push(term_id);
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
 	// Retrieves the glossary page and creates the templates for each of the terms from that content
 	var temp_div = $('<div>');
-	temp_div.load("../glossary/index.html", function() {
+	temp_div.load("../glossary", function() {
 		$.each(term_ids, function(index, term_id) {
 			var glossary_term_elements = $("#" + term_id, temp_div).nextUntil('h3');
 			var glossary_term_preview = glossary_term_elements[0];
